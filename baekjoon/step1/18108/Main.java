7d6dbeb151f75b605b3cc2efae92c047ff828b98
isaac <isaac7263@naver.com>
2023-09-03 22:57:25 +0900

    add: step1

---
+++ baekjoon/step1/18108/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int dif = 2541 - 1998;
+        int year = sc.nextInt();
+        System.out.println(year - dif);
+    }
+}
