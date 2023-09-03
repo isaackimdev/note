b5f8304bc78a310dbcba0189a65e0647d54d4ec6
isaac <isaac7263@naver.com>
2023-09-03 22:52:51 +0900

    add: step1

---
+++ baekjoon/step1/10926/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] srgs) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(name + "??!");
+    }
+}
