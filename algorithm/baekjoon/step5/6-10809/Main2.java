8ee5e08a8a435c3c4c9ef1a9ae81fd8a6257d82a
isaac <isaac7263@naver.com>
2023-09-23 11:28:06 +0900

    [a] step5 : 6-10809

---
+++ algorithm/baekjoon/step5/6-10809/Main2.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s = br.readLine();
+        for (char i = 'a'; i <= 'z'; i++) 
+            bw.write( s.indexOf(i) + " ");
+        bw.flush();
+        bw.close();
+    }
+}
