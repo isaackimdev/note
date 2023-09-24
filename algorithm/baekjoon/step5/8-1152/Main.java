a64c9f54e58cde171fff8da08cbc4814f445aca9
isaac <isaac7263@naver.com>
2023-09-25 08:33:01 +0900

    [a] step5 - 8-1152

---
+++ algorithm/baekjoon/step5/8-1152/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        StringTokenizer st = new StringTokenizer(s);
+        System.out.println(st.countTokens());
+    }
+}
