ce941b94b254b312f4cec3265efa7d42d0e2589f
isaac <isaac7263@naver.com>
2023-09-27 23:58:38 +0900

    [a] step5 11-11718, step6 1-25083

---
+++ algorithm/baekjoon/step5/11-11718/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s;
+        while(sc.hasNextLine()){
+            s = sc.nextLine();
+            System.out.println(s);
+        }
+    }
+}
---
+++ algorithm/baekjoon/step6/1-25083/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.print("      |");
+    }
+}
+
+/*
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+ */
