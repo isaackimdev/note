7c8a9a41f74d296299fa07dc7e41e41d708fea81
isaac <isaac7263@naver.com>
2023-10-21 11:03:03 +0900

    a: step9 2-2501

--- algorithm/baekjoon/step9/2-2501/Main.java
+++ algorithm/baekjoon/step9/2-2501/Main.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,20 +11,21 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
-        int seq = 0;
-        // logic
-        for(int i = 1; i <= n; i++) {
-            if (n % i == 0) { // 약수
-                System.out.println(i);
-                if (i == k) { 
-                    seq += 1;
-                    break;
-                }
-            } 
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int res = 0; 
+
+        ArrayList list = new ArrayList<Integer>();
+        for(int i=1; i<=n; i++) {
+            if(n%i == 0) {
+                list.add(i);
+            }
+        }
+        if(list.size() >= k) {
+            res = (int)list.get(k-1);
         }
 
-        bw.write(seq+"\n");
+        bw.write(res+"\n");
         bw.flush();
         bw.close();
     }
