62692208094bac08fd3071d5d6a1fa67663a2eb8
isaac <isaac7263@naver.com>
2023-10-23 23:27:51 +0900

    m: step9 5-2581 Main2로 update

--- algorithm/baekjoon/step9/5-2581/Main2.java
+++ algorithm/baekjoon/step9/5-2581/Main2.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 // 오답
-public class Main {
+public class Main2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
