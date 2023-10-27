8afdb46635f2e4f546c207cc8416c80d2a6ea2c2
isaac <isaac7263@naver.com>
2023-10-27 13:05:48 +0900

    m: step10 2-1085

--- algorithm/baekjoon/step10/2-1085/Main.java
+++ algorithm/baekjoon/step10/2-1085/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        StringTokenizer st = new StringTokenizer(sc.nextLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        int xmin = x < w - x ? x : w - x;
+        int ymin = y < h - y ? y : h - y;
+        int min = xmin < ymin ? xmin : ymin;
+        System.out.println(min);
     }
 }
