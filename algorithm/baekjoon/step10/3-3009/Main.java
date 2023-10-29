d9ac29c27b501a4a7d87e7dc879299d6006ea786
isaac <isaac7263@naver.com>
2023-10-30 00:01:29 +0900

    m: step10 3-3009

--- algorithm/baekjoon/step10/3-3009/Main.java
+++ algorithm/baekjoon/step10/3-3009/Main.java
@@ -11,14 +11,19 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int[][] dots = new int[3][2];
         
-
         for(int i=0; i<3; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             dots[i][0] = Integer.parseInt(st.nextToken());
             dots[i][1] = Integer.parseInt(st.nextToken());
         }
 
+        int x = dots[0][0] == dots[1][0] ? dots[2][0] : 
+            dots[0][0] == dots[2][0] ? dots[1][0] : dots[0][0];
+        int y = dots[0][1] == dots[1][1] ? dots[2][1] :
+            dots[0][1] == dots[2][1] ? dots[1][1] : dots[0][1];
         
-
+        bw.write(x+" "+y+"\n");
+        bw.flush();
+        bw.close();
     }
 }
