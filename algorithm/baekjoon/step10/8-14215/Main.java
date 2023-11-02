153ddeeb03cbfeaeb56ea247a55a983ebb7c6ac8
isaac <isaac7263@naver.com>
2023-11-03 08:13:51 +0900

    a: step10 8-14215

--- algorithm/baekjoon/step10/8-14215/Main.java
+++ algorithm/baekjoon/step10/8-14215/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        StringTokenizer st = new StringTokenizer(sc.nextLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int max = a;
+        int ex = b + c;
+        if (max < b) {
+            max = b;
+            ex = a + c;
+        }
+        if (max < c) {
+            max = c;
+            ex = a + b;
+        }
+        if (max >= ex) {
+            max -= (max - ex);
+            max --;
+        }
+        System.out.println(max+ex);
     }
 }
