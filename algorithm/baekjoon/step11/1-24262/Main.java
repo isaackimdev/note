295301ee8092449a614cdeabb3bb744cdb059d17
isaac <isaac7263@naver.com>
2023-11-10 18:02:47 +0900

    a: step11 1-24262

--- algorithm/baekjoon/step11/1-24262/Main.java
+++ algorithm/baekjoon/step11/1-24262/Main.java
@@ -1,8 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.next());
+        System.out.println(1);
+        System.out.println(0);
     }
 }
