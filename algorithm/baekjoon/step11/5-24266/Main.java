f1e7d934cb737bc1d4623edecee46ab42aa100f8
isaac <isaac7263@naver.com>
2024-01-25 23:39:33 +0900

    m

--- algorithm/baekjoon/step11/5-24266/Main.java
+++ algorithm/baekjoon/step11/5-24266/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println(n*n*n);
+        System.out.println(3);
     }
 }
