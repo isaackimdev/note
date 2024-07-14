a3f9100e541a51ea5c8dcf78807e8289f402897d
isaac <isaac7263@naver.com>
2024-07-14 23:13:00 +0900

    m

---
+++ algorithm/java/test/Greedy3.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Greedy3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        sc.nextLine();
+
+        // int[][] sheet = new int[n][m];
+        // for(int i = 0; i < n; i++) {
+        //     for (int j = 0; j < m; j++) {
+        //         sheet[i][j] = nextInt();
+        //     }
+        //     sc.nextLine();
+        // }
+
+        // System.out.println(Arrays.toString(sheet));
+    }
+}
